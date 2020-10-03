84ab4398bea810515fb159a6a0fde6e424da9f35
Sudini1412 <sudysan@gmail.com>
2020-10-03 17:23:17 +0100

    Update on anime_manga cog

--- albot/cogs/anime_manga.py
+++ albot/cogs/anime_manga.py
@@ -5,16 +5,42 @@ from discord.ext import commands
 
 COG_HELP = """TODO"""
 
-class Anime(commands.Cog):
+class Weeb(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.logging = logging.getLogger(__name__)
         
-    @commands.command(name='anime')
+    @commands.command(name='weeb')
     async def entry(self, context, *args):
-        await context.send(str(args))
+      
+    def anime(args[0]):
+        anime = []
+        if args:
+            name = args[0]
+        else:
+            await context.send("Bad args")
+        anime.apend(name)
+        print anime
+        return anime
+
+    def manga(args[0]):
+        manga = []
+        if args:
+            name = args[0]
+        else:
+            await context.send("Bad args")
+        anime.apend(name)
+        print manga
+        return manga
+
+    if args[0] == "anime":
+        anime(args[1])
+    if args[0] == "manga":
+        manga(args[1])
+
+    await context.send(str(args))
 
 def setup(bot):
     bot.add_cog(
-        Anime(bot)
+        Weeb(bot)
     )
