e82216b1c96029ca5cc0a3c45ba5c40c8e837f0a
Sudini The Great <sudinithegreat@Sudinis-MacBook-Pro.local>
2020-10-03 16:44:54 +0100

    This is an update to anime

---
+++ albot/cogs/anime_manga.py
@@ -0,0 +1,19 @@
+import logging
+
+import discord
+from discord.ext import commands
+
+COG_HELP = """TODO"""
+
+class Anime(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.logging = logging.getLogger(__name__)
+        
+    @commands.command(name='anime')
+	async def entry(self, context, *args):
+    	await context.send(str(args))
+def setup(bot):
+    bot.add_cog(
+        SomeCog(bot)
+    )
